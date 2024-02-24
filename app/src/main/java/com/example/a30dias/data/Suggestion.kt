bfb59716d0d34bcb9147cfac15cd98d97ec4f375
jusancc <jusancc@unirioja.es>
2024-02-24 09:42:05 +0100

    casi acabado

--- app/src/main/java/com/example/a30dias/data/Suggestion.kt
+++ app/src/main/java/com/example/a30dias/data/Suggestion.kt
@@ -2,4 +2,4 @@ package com.example.a30dias.data
 
 import androidx.annotation.DrawableRes
 
-data class Suggestion(val text: String, @DrawableRes val imageResource: Int)
+data class Suggestion(val text: String, @DrawableRes val imageResource: Int, val desc: String)
